029953af5d851f67113154fd5408207ddb0bca5d
Naif Muazzam <naifmuazzam03@gmail.com>
2026-07-31 16:38:34 +0800

    Make localhost HTTPS cert optional

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,19 @@
+import { existsSync } from 'node:fs'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const certPath = './outsource(ideaOnly)/localhost-cert'
+const hashCert = existsSync(certPath + '/localhost.pem') && existsSync(certPath + '/localhost-key.pem')
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    https: {
-      key: './outsource(ideaOnly)/localhost-cert/localhost-key.pem',
-      cert: './outsource(ideaOnly)/localhost-cert/localhost.pem',
-    },
-  },
+  server: hashCert
+    ? {
+        https: {
+          key: certPath + '/localhost-key.pem',
+          cert: certPath + '/localhost.pem',
+        },
+      }
+    : {},
 })
